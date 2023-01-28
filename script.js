a40b57eebe0bb845cf7b979247bea6b628e4e3c9
Andrey Chesnokov <113643060+Shad-Coder@users.noreply.github.com>
2023-01-28 21:16:51 +0300

    Add files via upload
    Version control - 1.1 - all changes are saved

---
+++ script.js
@@ -0,0 +1,32 @@
+const message = document.querySelector('.popup');
+const hand = document.querySelector('.disabled');
+const firstslide = document.querySelector('.first_slide');
+const twohand = document.querySelector('.disabled1');
+message.addEventListener('click', function () {
+    message.classList.remove('message1');
+    message.classList.add('onpopup');
+    hand.classList.remove('disabled')
+    hand.classList.add('enable')
+});
+firstslide.addEventListener('click', function () {
+firstslide.classList.remove('message');
+firstslide.classList.add('done_slide');
+twohand.classList.remove('disabled1')
+twohand.classList.add('enable1')
+});
+/* time - vremya */
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+    var time = h + ":" + m + ":" + s;
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").textContent = time;
+    time = document.querySelector('timer')
+    setTimeout(showTime, 1000);
+}
+showTime();
